997c0bd4b5faff7b7f3c9287af01158be816d949
Rameez <rameesmohd789@gmail.com>
2024-01-20 00:47:46 +0530

    added admin usermanagement apis

--- controller/adminController/adminController.js
+++ controller/adminController/adminController.js
@@ -52,6 +52,16 @@ const sendMail = async(email,OTP)=>{
     }
 }
 
+const resendOtp =async(req,res)=>{
+    try {
+        const bool = sendMail(req.body.email,req.body.OTP)
+        bool ? res.status(200).json({ message:'OTP sent successfully,please check your email' }) : res.status(500).json({message:'Error sending email'});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({})
+    }
+}
+
 const login = async( req,res)=>{
     try {
         console.log(req.body);
@@ -73,4 +83,44 @@ const login = async( req,res)=>{
     }
 }
 
-module.exports = { login }
+const fetchUsers=async(req,res)=>{
+    try {
+        const { search ,is_purchased ,is_completed}= req.query
+        const searchQuery = {
+            $or: [
+            { name: { $regex: new RegExp(search, 'i') } }, 
+            { email : { $regex: new RegExp(search, 'i') } }, 
+            ]
+        }
+        const query = {
+            ...(search !=='' && (isNaN(Number(search)) ? searchQuery : { mobile: Number(search) })),
+            ...(is_purchased && { is_purchased }),
+            ...(is_completed && { is_completed }),
+            is_admin : false
+        }
+        const users = await userModel.find(query,{password : 0})
+        res.status(200).json({result : users})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({})
+    }
+}
+
+const blockToggle=async(req,res)=>{
+    try {
+        console.log(req.body);
+        const {id,currStatus} = req.body
+        await userModel.updateOne({_id : id},{$set : {is_blocked : !currStatus}})
+        res.status(200).json({})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({})
+    }
+}
+
+module.exports = { 
+    login,
+    resendOtp,
+    fetchUsers,
+    blockToggle
+}
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const adminController = require('../controller/adminController/adminController')
 
 router.post('/login',adminController.login);
-
+router.route('/users')
+    .get(adminController.fetchUsers)
+    .patch(adminController.blockToggle)
 module.exports = router
