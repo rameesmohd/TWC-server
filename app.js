c4e9236eef50b6d7988eb14b5706dae44dbdd820
rameesmohd <rameesmohd789@gmail.com>
2024-02-02 22:04:18 +0530

    added cors options

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const createError = require('http-errors');
 
 connectdb();
 // app.use(cors({ credentials: true, origin: process.env.CLIENT_BASE_URL }));
-app.use(cors())
+app.use(cors({
+  origin: 'https://tradewalkeredu.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
