56a23449449f0a967a90e0e96c90a907eae02fc2
Rameez <rameesmohd789@gmail.com>
2024-02-29 20:14:29 +0530

    changed cors domain

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ dotenv.config();
 connectdb();
 // app.use(cors({ credentials: true, origin: process.env.CLIENT_BASE_URL }));
 app.use(cors({
-  origin: [ "https://www.tradewalkeredu.com","http://localhost:5173","https://twc-client.vercel.app"],
+  origin: [ "https://www.fourcapedu.com","http://localhost:5173","https://twc-client-pearl.vercel.app"],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
